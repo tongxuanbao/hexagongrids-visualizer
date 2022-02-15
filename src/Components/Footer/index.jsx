6daa27b200c27b3ccdb2ad3766360a3357cdb7be
Bao Tong <baotongxuan@gmail.com>
2022-02-15 12:32:16 +1000

    Security newtab

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -11,22 +11,39 @@ const Footer = () => {
   return (
     <footer>
       <div className="Footer">
-        <a href="mailto:baotongxuan@gmail.com" target="_blank">
+        <a
+          href="mailto:baotongxuan@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
-        <a href="https://github.com/tongxuanbao" target="_blank">
+        <a
+          href="https://github.com/tongxuanbao"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href="https://www.linkedin.com/in/baotongxuan/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/baotongxuan/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
         <a
           href="https://www.youtube.com/channel/UCa1KDC4kkpvGFmtCZDMP7EA"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <FontAwesomeIcon icon={faYoutube} />
         </a>
-        <a href="https://twitter.com/BaoTong99" target="_blank">
+        <a
+          href="https://twitter.com/BaoTong99"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FontAwesomeIcon icon={faTwitter} />
         </a>
       </div>
